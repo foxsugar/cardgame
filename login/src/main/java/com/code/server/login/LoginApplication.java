6aa7b5359e7f3c313bb28ea1ecfe8056d1c93171
foxsugar <redalert3@126.com>
2017-03-09 11:28:43 +0800

    测试修改

--- login/src/main/java/com/code/server/login/LoginApplication.java
+++ login/src/main/java/com/code/server/login/LoginApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"com.code.server.*"})
 public class LoginApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 	}
