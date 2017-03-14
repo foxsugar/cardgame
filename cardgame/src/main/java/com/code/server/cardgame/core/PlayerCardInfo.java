f9746353e802867e9b54209d8c239da9bf744d3f
foxsugar <redalert3@126.com>
2017-03-14 13:54:58 +0800

    逻辑代码

--- cardgame/src/main/java/com/code/server/cardgame/core/PlayerCardInfo.java
+++ cardgame/src/main/java/com/code/server/cardgame/core/PlayerCardInfo.java
@@ -1,12 +1,42 @@
 package com.code.server.cardgame.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by sunxianping on 2017/3/14.
  */
 public class PlayerCardInfo {
-    protected List<String> cards = new ArrayList<>();//手上的牌
-    protected List<String> disCards = new ArrayList<>();//丢弃的牌
+    protected List<Integer> cards = new ArrayList<>();//手上的牌
+    protected List<Integer> disCards = new ArrayList<>();//丢弃的牌
+
+
+
+    public void init(){
+        initCards();
+        //乱序
+        Collections.shuffle(cards);
+
+    }
+    protected void initCards(){
+        for(int i=1;i<=54;i++){
+            cards.add(i);
+        }
+        //去掉两张2
+        cards.remove((Integer)7);
+        cards.remove((Integer)8);
+    }
+
+    /**
+     * 发牌
+     */
+    protected void deal(){
+
+    }
+
+
+    public boolean isCanPlay(CardStruct cardStruct){
+
+    }
 }
