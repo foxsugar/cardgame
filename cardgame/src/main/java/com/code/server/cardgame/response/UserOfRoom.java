e298d31e2dbdcceb1694afcca5b3d2fce173b552
clarkkent <lykaka22@126.com>
2017-03-15 17:35:35 +0800

    1

---
+++ cardgame/src/main/java/com/code/server/cardgame/response/UserOfRoom.java
@@ -0,0 +1,45 @@
+package com.code.server.cardgame.response;
+
+import com.code.server.gamedata.UserVo;
+import net.sf.json.JSONObject;
+
+import java.util.List;
+
+public class UserOfRoom {
+    
+    //房间人数
+    private int inRoomNumber;
+    //准备好的人数
+    private int readyNumber;
+    
+    private List<UserVo> userList;
+    
+    
+    
+    public int getInRoomNumber() {
+		return inRoomNumber;
+	}
+
+	public void setInRoomNumber(int inRoomNumber) {
+		this.inRoomNumber = inRoomNumber;
+	}
+
+	public int getReadyNumber() {
+		return readyNumber;
+	}
+
+	public void setReadyNumber(int readyNumber) {
+		this.readyNumber = readyNumber;
+	}
+	
+	public List<UserVo> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<UserVo> userList) {
+		this.userList = userList;
+	}
+
+
+
+}
