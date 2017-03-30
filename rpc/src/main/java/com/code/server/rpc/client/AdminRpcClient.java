856fea22fe156012de244854c22675d0c47a08e4
foxsugar <redalert3@126.com>
2017-03-30 13:43:08 +0800

    admin rpc

--- rpc/src/main/java/com/code/server/rpc/client/AdminRpcClient.java
+++ rpc/src/main/java/com/code/server/rpc/client/AdminRpcClient.java
@@ -1,6 +1,6 @@
 package com.code.server.rpc.client;
 
-import com.code.server.rpc.idl.GameRPC;
+import com.code.server.rpc.idl.AdminRPC;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TTransport;
@@ -14,16 +14,16 @@ import static com.code.server.rpc.client.TransportManager.getTransport;
 public class AdminRpcClient {
 
 
-    public static GameRPC.Client getAClient(String host, int port) throws TTransportException {
+    public static AdminRPC.Client getAClient(String host, int port) throws TTransportException {
         TTransport transport = getTransport(host, port);
         TProtocol protocol = new TBinaryProtocol(transport);
-        GameRPC.Client client = new GameRPC.Client(protocol);
+        AdminRPC.Client client = new AdminRPC.Client(protocol);
         return client;
     }
 
-    public static GameRPC.Client getAClient(TTransport transport) throws TTransportException {
+    public static AdminRPC.Client getAClient(TTransport transport) throws TTransportException {
         TProtocol protocol = new TBinaryProtocol(transport);
-        GameRPC.Client client = new GameRPC.Client(protocol);
+        AdminRPC.Client client = new AdminRPC.Client(protocol);
         return client;
     }
 
