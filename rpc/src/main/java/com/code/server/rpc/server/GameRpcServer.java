681d624d8ba282d3fefc01f3236c446109cc1272
foxsugar <redalert3@126.com>
2017-04-07 16:41:51 +0800

    逻辑

--- rpc/src/main/java/com/code/server/rpc/server/GameRpcServer.java
+++ rpc/src/main/java/com/code/server/rpc/server/GameRpcServer.java
@@ -3,6 +3,7 @@ package com.code.server.rpc.server;
 import com.code.server.rpc.idl.GameRPC;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TNonblockingServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadedSelectorServer;
 import org.apache.thrift.transport.TFramedTransport;
@@ -15,15 +16,25 @@ import org.apache.thrift.transport.TTransportException;
 public class GameRpcServer {
 
     public static TServer StartServer(int port,GameRPC.AsyncIface iface) throws TTransportException {
-        TProcessor tprocessor = new GameRPC.AsyncProcessor<>(iface);
-        TNonblockingServerSocket serverTransport = null;
-        serverTransport = new TNonblockingServerSocket(port);
-        TThreadedSelectorServer.Args tArgs = new TThreadedSelectorServer.Args(serverTransport);
-        tArgs.processor(tprocessor);
-        tArgs.transportFactory(new TFramedTransport.Factory());
-        tArgs.protocolFactory(new TBinaryProtocol.Factory());
-        TServer server = new TThreadedSelectorServer(tArgs);
-        server.serve();
+        TServer server = null;
+        try {
+
+            TProcessor tprocessor = new GameRPC.AsyncProcessor<>(iface);
+            TNonblockingServerSocket serverTransport = null;
+            serverTransport = new TNonblockingServerSocket(port);
+            TThreadedSelectorServer.Args tArgs = new TThreadedSelectorServer.Args(serverTransport);
+            tArgs.processor(tprocessor);
+            tArgs.transportFactory(new TFramedTransport.Factory());
+            tArgs.protocolFactory(new TBinaryProtocol.Factory());
+
+
+            server = new TThreadedSelectorServer(tArgs);
+            server.serve();
+
+        } catch (Exception e) {
+            System.out.println("-------------------------------------");
+        e.printStackTrace();
+        }
         return server;
     }
 
