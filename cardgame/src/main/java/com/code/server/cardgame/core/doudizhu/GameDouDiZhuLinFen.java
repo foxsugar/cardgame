539f9d4bce4227c6443ba49f2a98e88f233a31ce
foxsugar <redalert3@126.com>
2017-05-22 13:28:57 +0800

    斗地主逻辑

--- cardgame/src/main/java/com/code/server/cardgame/core/doudizhu/GameDouDiZhuLinFen.java
+++ cardgame/src/main/java/com/code/server/cardgame/core/doudizhu/GameDouDiZhuLinFen.java
@@ -14,9 +14,14 @@ public class GameDouDiZhuLinFen extends GameDouDiZhu{
     private static final Logger logger = LoggerFactory.getLogger(GameDouDiZhuLinFen.class);
 
 
-    protected int initCardNum = 16;
 
 
+
+    @Override
+    public void init(List<Long> users, long dizhuUser) {
+        this.initCardNum = 16;
+        super.init(users,dizhuUser);
+    }
 
     /**
      * 洗牌
