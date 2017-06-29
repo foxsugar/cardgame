961569cbfadfae069507b582a2a85452cbde1287
foxsugar <redalert3@126.com>
2017-06-29 17:53:33 +0800

    增加rpc接口

--- rpc/src/main/thrift/gameRpc.thrift
+++ rpc/src/main/thrift/gameRpc.thrift
@@ -52,7 +52,13 @@ service GameRPC{
    //是否苹果审查
    i32 modifyAppleCheck(i32 status),
 
-   i32 modifyDownload2(string str)
+   i32 modifyDownload2(string str),
+
+   i32 addBlackList(i64 userId),
+
+   i32 removeBlackList(i64 userId),
+
+   set<i64> getBlackList();
 
    OnlineNum getOnlineUser()
 }
